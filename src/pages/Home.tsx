e4f174eb51d4e2482fbae1ebee0db37d187ae988
Nataniel_Andrade <natanieljr78@gmail.com>
2026-06-30 16:58:14 +0200

    Initial commit: The basic structure

---
+++ src/pages/Home.tsx
@@ -0,0 +1,9 @@
+export default function Home() {
+  return (
+    <div className="Home">
+      <h1 className="text-3xl font-bold underline">
+        Welcome to the Home Page!
+      </h1>
+    </div>
+  )
+}
